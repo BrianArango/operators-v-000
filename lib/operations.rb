dbc3f78a212789bedb38b7c4fd06dadf2fdd92a8
Brian Arango <brianvictus@gmail.com>
2020-07-09 20:31:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -6,14 +6,13 @@ def unsafe?(speed)
   if speed < 40
     return true
   end
-=end
-  
   if speed > 60 && speed < 40
     return true
   else
     false 
   end
 end
+=end 
 
 def not_safe?(speed)
   if speed > 60
